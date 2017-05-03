11a19bdd67c132a4344019246507496794601283
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-05-03 09:54:46 +0200

    Step 2 : replaced HttpServletRequest by @RequestHeader HttpHeaders.

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,12 +15,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class RootController {
 
   @RequestMapping("/")
-  public ModelAndView test(HttpServletRequest request) {
+  public ModelAndView test(@RequestHeader HttpHeaders headers) {
     String headerName = "headerName";
-    List<String> headers = new ArrayList<String>(Collections.list(request.getHeaderNames()));
+    List<String> headerNames = new ArrayList<String>(headers.keySet());
 
     ModelAndView mav = new ModelAndView();
-    mav.addObject(headerName, headers);
+    mav.addObject(headerName, headerNames);
     mav.addObject("titre", "IUT");
     mav.addObject("corps", "bonjour");
     mav.setViewName("template");
