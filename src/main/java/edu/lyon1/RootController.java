f655799e5f3119a233dc5aad961bb2ab5297394e
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-05-03 09:41:07 +0200

    Correcting code in order to pass test 1.

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -1,5 +1,8 @@
 package edu.lyon1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +14,11 @@ public class RootController {
 
   @RequestMapping("/")
   public ModelAndView test(HttpServletRequest request) {
+    String headerName = "headerName";
+    List<String> headers = new ArrayList<String>(Collections.list(request.getHeaderNames()));
+
     ModelAndView mav = new ModelAndView();
+    mav.addObject(headerName, headers);
     mav.addObject("titre", "IUT");
     mav.addObject("corps", "bonjour");
     mav.setViewName("template");
